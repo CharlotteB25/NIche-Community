d73a58b5b376a804c352b13b6f9b801a43f95a3d
CharlotteB25 <charbill@student.arteveldehs.be>
2022-11-03 18:28:46 +0100

    03/11

--- OneDrive/Desktop/Artevelde/semester6/MobDev1/project1/opdracht01-dev-quiz-CharlotteB25/parcel/src/app.js
+++ OneDrive/Desktop/Artevelde/semester6/MobDev1/project1/opdracht01-dev-quiz-CharlotteB25/parcel/src/app.js
@@ -1,24 +1,61 @@
-/**
- * The Main Application
- */
+import Quiz from './quiz.js';
 
-import { APP_TITLE } from './consts.js';
+class Settings {
+  constructor() {
+    this.quizElement = document.querySelector('.quiz');
+    this.settingsElement = document.querySelector('.settings');
+    this.category = document.querySelector('#category');
+    this.numberOfQuestions = document.querySelector('#questions');
+    this.difficulty = [
+      document.querySelector('#easy'),
+      document.querySelector('#medium'),
+      document.querySelector('#hard'),
+    ];
+    this.startButton = document.querySelector('#start');
 
-const app = () =>
-{
-  // set the app title
-  document.title = APP_TITLE;
+    this.quiz = { };
 
-  // TODO: create your application
+    this.startButton.addEventListener('click', this.startQuiz.bind(this));
+  }
 
-  const textContainer = document.createElement('div');
-  textContainer.className = "text-container";
-  //textContainer.innerHTML = "A Basic JavaScript Starter Template"
+  async startQuiz() {
+    try {
+      const amount = this.getAmount();
+      const categoryId = this.category.value;
+      const difficulty = this.getCurrentDifficulty();
 
-  // add to the app container
-  const appContainer = document.getElementById('app');
-  appContainer.append(textContainer);
-};
+      const url = `https://quizapi.io/api/v1/questions?apiKey=RLeUA0WA2MKxtzCUFicrg3c9nLHHxO5sZ8RCHqQW&category=${categoryId}&difficulty=${difficulty}&limit=${amount}`;
 
-// start the app
-app();
+      let data = await this.fetchData(url);
+      this.quiz = new Quiz(this.quizElement, amount, data.results);
+    } catch (error) {
+      alert(error);
+    }
+  }
+  async fetchData(url) {
+    const response = await fetch(url);
+    const result = await response.json();
+
+
+    return result;
+  }
+
+  getCurrentDifficulty() {
+    const checkedDifficulty = this.difficulty.filter(element => element.checked);
+
+    if (checkedDifficulty.length === 1) {
+      return checkedDifficulty[0].id;
+    } 
+  }
+
+  getAmount() {
+    const amount = this.numberOfQuestions.value;
+    // Not negative, not 0 and not over 50
+    if (amount > 0 && amount < 51) {
+      return amount;
+    }
+    throw new Error('Please enter a number of questions between 1 and 50!');
+  }
+}
+
+export default Settings;
